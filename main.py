239c4bad6ad8ef8f379d5af49b86f8fefe09b92e
abhijithvk191 <abijithvjn@gmail.com>
2017-04-06 10:39:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 #import subprocess
 from function import *
 import os.path
@@ -26,16 +27,16 @@ def func_hostManagement():
         print(' 4.List all host')
         print(' 5.Quit.')
         print('')
-        hm_option = int(input(' Select Option : '))
-        if hm_option == 1:
+        hm_option = input(' Select Option : ')
+        if hm_option == '1':
             fun_addNewHost()
-        elif hm_option == 2:
+        elif hm_option == '2':
             fun_delExistingHost()
-        elif hm_option == 3 or hm_option == '3':
+        elif hm_option == '3' or hm_option == '3':
             fun_changeSettings()
-        elif hm_option == 4:
+        elif hm_option == '4':
             fun_ListAllHost()
-        elif hm_option == 5:
+        elif hm_option == '5':
             break
         else:
             print('')
@@ -64,9 +65,10 @@ def func_groupManagement():
 		print('',)
 		print(' 1.Add New Group.')
 		print(' 2.Delete Exisitng Group.')
-		print(' 3.Group Management')
-		print(' 4.List all Groups')
-		print(' 5.Quit')
+		print(' 3.Add servers to group')
+		print(' 4.Delete servers from group')
+		print(' 5.List all Groups')
+		print(' 6.Quit')
 		print('')
 		hm_option = int(input(' Select Option : '))
 		if hm_option == 1:
@@ -74,10 +76,12 @@ def func_groupManagement():
 		elif hm_option == 2:
 			fun_delExistingGroup()
 		elif hm_option == 3:
-		  fun_groupManage()
+			fun_addservertoGroup()
 		elif hm_option == 4:
-			fun_listGroups()
+			fun_deletefomGroup()			
 		elif hm_option == 5:
+			fun_listGroups()
+		elif hm_option == 6:
 			break
 		else:
 			print('')
@@ -93,11 +97,11 @@ def func_groupManagement():
 
 while True:
 	fun_clearScreen()
-#	if os.path.exists("./server.json"):
-#		pass
-#	else:
-#		subprocess.call('touch server.json',shell=True)
-#		subprocess.call("echo '{}' > server.json",shell=True)
+	if os.path.exists("./server.json"):
+		pass
+	else:
+		subprocess.call('touch server.json',shell=True)
+		subprocess.call("echo '{}' > server.json",shell=True)
     
 	print()
 	msg = 'parallel command execution'
@@ -110,19 +114,20 @@ while True:
 	print(' 4.Parallel Execution.')
 	print(' 5.Quit ')
 	print('')
-	OPTION = int(input(' Select Option : '))
+	OPTION = input(' Select Option : ')
     
-	if OPTION == 1:
+	if OPTION == '1':
 		func_hostManagement()
-	elif OPTION == 2:
+	elif OPTION == '2':
 		func_groupManagement()
-	elif OPTION == 3 or OPTION == '3':
+	elif OPTION == '3' or OPTION == '3':
 		func_quickServerAccess()
-	elif OPTION == 4:
+	elif OPTION == '4':
 		func_parallelExecution()
-	elif OPTION == 5:
+	elif OPTION == '5':
 		break
 	else:
 		print('')
 		input(' Invalid Option,Enter To continue.')     
         
+
